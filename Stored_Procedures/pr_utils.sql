bae4a8061e13432a34fdbcb3c4f78aad96d16fc7
Almeida-a <aralmeida99@gmail.com>
2021-06-19 22:53:45 +0100

    SP for getting type of given vehicle

---
+++ Stored_Procedures/pr_utils.sql
@@ -0,0 +1,70 @@
+-- Utility functions
+
+
+-- Check which type is the given vehicle
+-- input: serial number
+-- output: either "Tube", "Train", "Bus", "Neither", "Inexistent"
+
+-- Create a new stored procedure called 'getVehicleType' in schema 'dbo.Transportation_System'
+-- Drop the stored procedure if it already exists
+IF EXISTS (
+SELECT *
+    FROM INFORMATION_SCHEMA.ROUTINES
+WHERE SPECIFIC_SCHEMA = N'dbo.Transportation_System'
+    AND SPECIFIC_NAME = N'getVehicleType'
+)
+DROP PROCEDURE dbo.Transportation_System.getVehicleType
+GO
+-- Create the stored procedure in the specified schema
+CREATE PROCEDURE dbo.Transportation_System.getVehicleType
+    @serialNo int = -1,
+    @type varchar(10) OUTPUT
+AS
+    -- body of the stored procedure
+    SELECT @serialNo;
+
+    --- Error handling
+    -- @serialNo = -1? Raiserror 16
+    IF @serialNo=-1
+    BEGIN
+        RAISERROR 16 'Proc getVehicleType: No vehicle specified';
+        RETURN -1;
+    END;
+
+    -- Exists in tube?
+    IF (@serialNo IN (SELECT SerialNo FROM Transportation_System.Tube))
+    BEGIN
+        -- save in output variable
+        SET @type = "Tube";
+        RETURN 0;
+    END;
+    -- Exists in bus?
+    IF (@serialNo IN (SELECT SerialNo FROM Transportation_System.Bus))
+    BEGIN
+        -- save in output variable
+        SET @type = "Bus";
+        RETURN 0;
+    END;
+    -- Exists in train?
+    IF (@serialNo IN (SELECT SerialNo FROM Transportation_System.Train))
+    BEGIN
+        -- save in output variable
+        SET @type = "Train";
+        RETURN 0;
+    END;
+    -- Exists at all?
+    IF(@serialNo IN (SELECT SerialNo FROM Transportation_System.PublicVehicle))
+    BEGIN
+        SET @type = 'Inexistent';
+        RETURN 0;
+    END
+    ELSE
+    BEGIN
+        SET @type = 'Neither';
+        RETURN 0;
+    END
+    
+GO
+-- example to execute the stored procedure we just created
+EXECUTE dbo.Transportation_System.getVehicleType 0;
+GO;
