9c5e86c3274843967536d4caed6b40462ec20aad
Almeida-a <aralmeida99@gmail.com>
2021-06-22 17:51:27 +0100

    Added 2 functional views

--- Views/vehicles.sql
+++ Views/vehicles.sql
@@ -9,6 +9,34 @@
         group by serialno, modelId, capacity
         order by tripQtt
 */
+-- Create a new view called 'TripQuant' in schema 'Transportation_System'
+-- Drop the view if it already exists
+IF EXISTS (
+SELECT *
+    FROM sys.views
+    JOIN sys.schemas
+    ON sys.views.schema_id = sys.schemas.schema_id
+    WHERE sys.schemas.name = N'Transportation_System'
+    AND sys.views.name = N'TripQuant'
+)
+DROP VIEW Transportation_System.TripQuant
+GO
+-- Create the view in the specified schema
+CREATE VIEW Transportation_System.TripQuant
+AS
+    -- body of the view
+    SELECT SerialNo, ModelID, Capacity, COUNT(TripNo) AS TripQtt
+    FROM Transportation_System.PublicVehicle AS pv
+	-- left outer -> Includes vehicles without trip => tripQtt = 0
+        LEFT OUTER JOIN Transportation_System.Trip AS t ON pv.SerialNo=t.VehicleSerialNo
+    GROUP BY SerialNo, ModelID, Capacity;
+GO
+
+-- Test query
+select * from Transportation_System.TripQuant;
+-- drop (it interferes with other batches if this is not run - not critical)
+DROP VIEW Transportation_System.TripQuant;
+
 
 -- Number of stops
 /*
@@ -24,3 +52,39 @@
         group by serialno, modelId, capacity
         order by desc stopQtt
 */
+
+
+-- Vehicles without a subtype
+-- Create a new view called 'AbstractVehicles' in schema 'Transportation_System'
+-- Drop the view if it already exists
+IF EXISTS (
+SELECT *
+    FROM sys.views
+    JOIN sys.schemas
+    ON sys.views.schema_id = sys.schemas.schema_id
+    WHERE sys.schemas.name = N'Transportation_System'
+    AND sys.views.name = N'AbstractVehicles'
+)
+DROP VIEW Transportation_System.AbstractVehicles
+GO
+-- Create the view in the specified schema
+CREATE VIEW Transportation_System.AbstractVehicles
+AS
+    -- body of the view
+    SELECT *
+    FROM Transportation_System.PublicVehicle
+    WHERE SerialNo NOT IN (
+        -- Bus
+        SELECT SerialNo FROM Transportation_System.Bus
+        UNION
+        -- Train
+        SELECT SerialNo FROM Transportation_System.Train
+        UNION 
+        -- Tube
+        SELECT SerialNo FROM Transportation_System.Tube
+    )
+GO
+
+select * from Transportation_System.AbstractVehicles;
+
+DROP VIEW Transportation_System.AbstractVehicles;
